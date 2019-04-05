83be80ae0b3553e664e8eafb7a61f4860be9ca3d
Carlil Jean <jaedagk@live.com>
2019-04-04 22:00:00 -0700

    Final.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
 // CODE here for your Lambda Classes
+class Lambda {
+    constructor(object){
+        this.name = object.name;
+        this.age = object.age;
+        this.location = object.location;
+    }
+    sayHello(){
+        console.log(`Hello, ${this.name}!`);
+    }
+}
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,39 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+class GameObject{
+    constructor(object){
+        this.createdAt = object.createdAt;
+        this.name = object.name;
+        this.dimensions = object.dimesions;
+        }
+    destroy(){
+        return `${this.name} was removed from the game.`;
+    }
+}
+
+class CharacterStats extends GameObject{
+    constructor(object){
+        super(object);
+        this.healthPoints = object.healthPoints;
+    }
+    takeDamage() {
+        GameObject.destroy();
+    }
+}
+
+class Humanoid extends CharacterStats{
+    constructor(object){
+        this.team = object.team;
+        this.weapons = object.weapons;
+        this.language = object.language;
+    }
+        greet() {
+            return `${this.name} offers a greeting in ${this.language}.`;
+        }
+        destroy(){
+            CharacterStats.takeDamage;
+        }
+    }
+}
